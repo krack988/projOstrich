356f34f6822cd2f38756675c42cefbdebe19647a
Mihail P <mihail.pantuyhin@alseda.by>
2023-03-08 12:28:23 +0300

    add some utils methods

--- app/src/main/java/com/example/ostrichp/utils/Utils.kt
+++ app/src/main/java/com/example/ostrichp/utils/Utils.kt
@@ -3,8 +3,15 @@ package com.example.ostrichp.utils
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.res.Resources
 import android.net.Uri
 import android.provider.Settings
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.URLSpan
+import androidx.core.content.res.ResourcesCompat
+import com.example.ostrichp.R
 
 object Utils {
 
@@ -27,4 +34,38 @@ object Utils {
         }.getOrDefault(false)
     }
 
+    fun CharSequence?.makeSelectedAfterFirstSpaces(
+        resources: Resources,
+        spaces: Int = 1
+    ): CharSequence? {
+        if (this == null) return this
+        if (spaces < 1) return this
+        var startPos = -1
+        var count = 0
+        this.forEachIndexed { index, c ->
+            if (c == ' ') {
+                count++
+                if (count == spaces) startPos = index
+            }
+        }
+        return if (startPos in this.indices) {
+            val result = SpannableString(this)
+            result.setSpan(
+                ForegroundColorSpan(
+                    ResourcesCompat.getColor(
+                        resources,
+                        R.color.error,
+                        null
+                    )
+                ), startPos, this.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            result
+        } else this
+    }
+
+}
+
+fun String?.format2Lines(): String? {
+    return if (this.orEmpty().contains('\n')) this
+    else this?.replaceFirst(' ', '\n')
 }
